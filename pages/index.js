d022345cdd61f3343349094957128bca15af7aed
Espen Strømland <espen.stromland@skatteetaten.no>
2019-01-11 13:59:33 +0100

    updated to gatsby v2

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,16 @@ import { graphql } from 'gatsby'
 import Grid from 'aurora-frontend-react-komponenter/Grid'
 import NavigationTile from 'aurora-frontend-react-komponenter/NavigationTile'
 import { SingleColumnRow } from 'starter/components/Columns'
-import Layout from 'starter/components/Layout'
 
 const IndexPage = ({ data: { site } }) => (
-  <Layout>
-    <Grid>
-      <SingleColumnRow>
-        <h2>Velkommen til Skatteetatens github</h2>
-      </SingleColumnRow>
-      <SingleColumnRow>
-        <NavigationTile contents={site.siteMetadata.repos} />
-      </SingleColumnRow>
-    </Grid>
-  </Layout>
+  <Grid>
+    <SingleColumnRow>
+      <h2>Velkommen til Skatteetatens github</h2>
+    </SingleColumnRow>
+    <SingleColumnRow>
+      <NavigationTile contents={site.siteMetadata.repos} />
+    </SingleColumnRow>
+  </Grid>
 )
 
 export default IndexPage
